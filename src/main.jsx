f979191b0b9060f8b97ceb7a68930c7df8cb844d
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-05 00:29:08 +0530

    pushing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import { ResumeEditor } from './components/resumeEditor'
+import { ResumePreview } from './components/resumePreview'
+
+import "./styles/fontColour.css"
+import "./styles/main.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ResumeEditor />
+    <ResumePreview />
   </StrictMode>,
 )
