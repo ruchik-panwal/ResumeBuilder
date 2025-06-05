567605a2748c83fe6f495763addb505f76816aa3
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-06 01:27:02 +0530

    made a component for the form of personal details.. this takes the details and returns an object containing them

--- src/components/resumeEditor.jsx
+++ src/components/resumeEditor.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { ResumePreview } from "./resumePreview";
 import "/src/styles/resumeEditor.css";
 
 export function ResumeEditor() {
@@ -8,32 +10,94 @@ export function ResumeEditor() {
   );
 }
 
-function clearBtn(e) {
-  e.preventDefault();
+
+
+// This Function Handles the persnal detail Form
+function PersonalDetails() {
+  const [value, setValue] = useState({
+    personName: "",
+    personEmail: "",
+    personNumber: "",
+  });
+
+  function dataCollector(val, id) {
+    let details = { ...value };
+    for (const key in details) {
+      if (key == id) details[key] = val;
+    }
+    setValue(details);
+  }
+
+  function clearBtn() {
+    let details = { ...value };
+    for (const key in details) {
+      details[key] = "";
+    }
+    setValue(details);
+  }
+
+  function submitBtn(obj) {
+  console.log(obj);
 }
 
-function PersonalDetails() {
   return (
     <form>
       <h1>Personal Details</h1>
       <div>
         <label htmlFor="personName">Name: </label>
-        <input type="text" id="personName" />
+        <input
+          type="text"
+          id="personName"
+          value={value.personName}
+          onChange={(event) =>
+            dataCollector(event.target.value, event.target.id)
+          }
+        />
       </div>
 
       <div>
         <label htmlFor="personEmail">Email: </label>
-        <input type="email" id="personEmail" />
+        <input
+          type="email"
+          id="personEmail"
+          value={value.personEmail}
+          onChange={(event) =>
+            dataCollector(event.target.value, event.target.id, event.id)
+          }
+        />
       </div>
 
       <div>
         <label htmlFor="personNumber">Contact: </label>
-        <input type="text" id="personNumber" />
+        <input
+          type="text"
+          id="personNumber"
+          value={value.personNumber}
+          onChange={(event) =>
+            dataCollector(event.target.value, event.target.id, event.id)
+          }
+        />
       </div>
 
       <div className="buttonWrap">
-        <button onClick={clearBtn}>Clear All</button>
-        <button type="submit">Save</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            clearBtn();
+          }}
+        >
+          Clear All
+        </button>
+
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            submitBtn(value);
+          }}
+        >
+          Save
+        </button>
       </div>
     </form>
   );
