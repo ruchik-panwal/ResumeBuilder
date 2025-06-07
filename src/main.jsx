2a0a3ab58fd5332cba52f873caadc3496a6184de
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-08 00:37:09 +0530

    made App.js which act as a parent component too enable sharing data between preview and editor

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import { ResumeEditor } from './components/resumeEditor'
-import { ResumePreview } from './components/resumePreview'
+import { App } from "./App";
 
-import "./styles/fontColour.css"
-import "./styles/main.css"
+import "./styles/fontColour.css";
+import "./styles/main.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ResumeEditor />
-    <ResumePreview />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
