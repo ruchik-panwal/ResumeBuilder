fcc83fa830dcf35a219f4d6a1271df6ec91294e1
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-10 00:39:56 +0530

    using use state to track the resume data nd transfer the changed data to preview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import { ResumeEditor } from './components/resumeEditor'
 import { ResumePreview } from './components/resumePreview'
+import { mainData } from './components/mainData';
 
 export function App(){
+    const [resumeData, setResumeData] = useState(mainData);
+    
+    function appendingData(givenData){
+        setResumeData(givenData);
+    }
 
     return <>
-        <ResumeEditor  />
-        <ResumePreview />
+        <ResumeEditor getResumeData = {appendingData} previewData={resumeData} />
+        <ResumePreview previewData = {resumeData} />
     </>
 }
