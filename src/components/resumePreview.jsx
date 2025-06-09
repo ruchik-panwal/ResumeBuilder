d7a2efaebf058b6ccfe7bb73d38f413c49f685ba
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-10 00:38:40 +0530

    added a function for building dom for personal details and takes the value from the parents

--- src/components/resumePreview.jsx
+++ src/components/resumePreview.jsx
@@ -1,5 +1,40 @@
 import "/src/styles/resumePreview.css";
 
-export function ResumePreview() {
-  return <div className="a4Preview"></div>;
+export function ResumePreview({ previewData }) {
+  return (
+    <div className="a4Preview">
+      <PerDetailsBuilder
+        personalData={previewData[0]}
+        previewData={previewData}
+      />
+    </div>
+  );
+}
+
+function PerDetailsBuilder({ personalData, previewData }) {
+  if (personalData?.children.length == 0) {
+    return null;
+  }
+
+  let domObj = previewData[personalData?.children[0]];
+
+  if (
+    domObj.personName == "" &&
+    domObj.personNumber == "" &&
+    domObj.personEmail == ""
+  )
+    return null;
+
+  return (
+    <div className="personalDetails">
+      {domObj.personName && <h1>{domObj.personName}</h1>}
+
+      {(domObj.personEmail || domObj.personNumber) && (
+        <div>
+          {domObj.personEmail && <p>{domObj.personEmail}</p>}
+          {domObj.personNumber && <p>{domObj.personNumber}</p>}
+        </div>
+      )}
+    </div>
+  );
 }
