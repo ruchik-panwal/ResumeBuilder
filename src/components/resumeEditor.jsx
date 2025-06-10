ad32b52d8a7dd3565a99791af60c6f1aab58626d
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-11 01:31:08 +0530

    created exp, ed, and project Forms dynamically and made the clear btn working

--- src/components/resumeEditor.jsx
+++ src/components/resumeEditor.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "/src/styles/resumeEditor.css";
 
+import { educationInputs, experienceInputs, projectInputs } from "./mainData";
+
 export function ResumeEditor({ getResumeData, previewData }) {
   const [counter, setCounter] = useState(5);
 
   // Stores the Data taken from the form to the resume Data updating the state
   function printValue(val, id) {
-    let newData = {...previewData};
+    let newData = { ...previewData };
 
-    // 
+    //
     if (newData[id]?.children?.length != 0) {
       newData[newData[id]?.children[0]] = val;
     } else {
@@ -22,40 +24,26 @@ export function ResumeEditor({ getResumeData, previewData }) {
   // Returning the data from the form
   return (
     <div className="resumeEditor">
-      <PersonalDetails onBtnClick={printValue} />
+      <Personal onBtnClick={printValue} />
+      <Experience />
+      <Projects />
+      <Education />
+      {/* <Skills /> */}
     </div>
   );
 }
 
 // This Function Handles the personal detail Form
-function PersonalDetails({ onBtnClick }) {
-  // Thsi UseState checks on the value of inputs
+function Personal({ onBtnClick }) {
+  // This UseState checks on the value of inputs
   const [value, setValue] = useState({
     personName: "",
     personEmail: "",
     personNumber: "",
   });
 
-  // This changes the input field and stores the state
-  function inputChange(val, id) {
-    let details = { ...value };
-    for (const key in details) { 
-      if (key == id) details[key] = val;
-    }
-    setValue(details);
-  }
-
-  // Clears the Input fields when clicked
-  function clearBtn() {
-    let details = { ...value };
-    for (const key in details) {
-      details[key] = "";
-    }
-    setValue(details);
-  }
-
   return (
-    <form>
+    <form className="personalWrap">
       <h1>Personal Details</h1>
       <div>
         <label htmlFor="personName">Name: </label>
@@ -63,7 +51,9 @@ function PersonalDetails({ onBtnClick }) {
           type="text"
           id="personName"
           value={value.personName}
-          onChange={(event) => inputChange(event.target.value, event.target.id)}
+          onChange={(event) =>
+            setValue(inputChange(event.target.value, event.target.id, value))
+          }
         />
       </div>
 
@@ -74,7 +64,7 @@ function PersonalDetails({ onBtnClick }) {
           id="personEmail"
           value={value.personEmail}
           onChange={(event) =>
-            inputChange(event.target.value, event.target.id, event.id)
+            setValue(inputChange(event.target.value, event.target.id, value))
           }
         />
       </div>
@@ -86,7 +76,7 @@ function PersonalDetails({ onBtnClick }) {
           id="personNumber"
           value={value.personNumber}
           onChange={(event) =>
-            inputChange(event.target.value, event.target.id, event.id)
+            setValue(inputChange(event.target.value, event.target.id, value))
           }
         />
       </div>
@@ -95,7 +85,11 @@ function PersonalDetails({ onBtnClick }) {
         <button
           onClick={(e) => {
             e.preventDefault();
-            clearBtn();
+            setValue({
+              personName: "",
+              personEmail: "",
+              personNumber: "",
+            });
           }}
         >
           Clear All
@@ -109,6 +103,107 @@ function PersonalDetails({ onBtnClick }) {
           }}
         >
           Save
+        </button>
+      </div>
+    </form>
+  );
+}
+
+function Experience() {
+  return (
+    <div className="experienceWrap">
+      <h1>Experience Details</h1>
+      <FormInpBuilder
+        uid="exp1"
+        inpField={Object.keys(experienceInputs)}
+        Input={experienceInputs}
+      />
+      <button>Add</button>
+    </div>
+  );
+}
+
+function Education() {
+  return (
+    <div className="educationWrap">
+      <h1>Education Details</h1>
+      <FormInpBuilder
+        uid="ed1"
+        inpField={Object.keys(educationInputs)}
+        Input={educationInputs}
+      />
+      <button>Add</button>
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div className="projectsWrap">
+      <h1>Project Details</h1>
+      <FormInpBuilder
+        uid="p1"
+        inpField={Object.keys(projectInputs)}
+        Input={projectInputs}
+      />
+      <button>Add</button>
+    </div>
+  );
+}
+
+// This changes the input field and returns new object to change the state.
+function inputChange(val, id, value) {
+  let details = { ...value };
+  for (const key in details) {
+    if (key == id) details[key] = val;
+  }
+  return details;
+}
+
+// Builds the DOM for exp, edu and project form
+function FormInpBuilder({ uid, inpField, Input }) {
+  const [value, setValue] = useState(Input);
+
+  return (
+    <form>
+      {inpField.map((lab) => {
+        const newID = uid + lab; // Creating a unique key
+
+        return (
+          <div key={newID}>
+            <label htmlFor={newID}>{lab} : </label>
+            <input
+              type="text"
+              id={newID}
+              className={lab}
+              value={value[lab] || ""}
+              onChange={(event) =>
+                setValue(
+                  inputChange(event.target.value, event.target.className, value)
+                )
+              }
+            ></input>
+          </div>
+        );
+      })}
+
+      <div className="buttonWrap">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setValue(Input);
+          }}
+        >
+          Clear All
+        </button>
+
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
+          Submit
         </button>
       </div>
     </form>
