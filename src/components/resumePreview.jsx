dc58d6405c377064d36493ebaf4aacf645cdd5dd
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-12 16:58:58 +0530

    added all previews..not optimisez

--- src/components/resumePreview.jsx
+++ src/components/resumePreview.jsx
@@ -6,6 +6,9 @@ export function ResumePreview({ previewData }) {
     <div className="a4Preview">
       <PerDetailsBuilder previewData={previewData} />
       <ExpBuilder previewData={previewData} />
+      <EduBuilder previewData={previewData} />
+      <ProBuilder previewData={previewData} />
+      <SkiBuilder previewData={previewData} />
     </div>
   );
 }
@@ -28,7 +31,7 @@ function PerDetailsBuilder({ previewData }) {
     return null;
 
   return (
-    <div className="personalDetails">
+    <div className="perDetails">
       {domObj.personName && <h1>{domObj.personName}</h1>}
 
       {(domObj.personEmail || domObj.personNumber) && (
@@ -48,21 +51,101 @@ function ExpBuilder({ previewData }) {
   if (!expChild.length) return null;
 
   return (
-    <div className="experienceDetails">
+    <div className="expDetails">
       <h3 className="detailsHeader">Work Experience</h3>
 
-      {expChild.map((child) => {
+      {expChild.map((child, idx) => {
         return (
-          <div className="workExp">
+          <div key={idx} className="workExp">
             <div className="titleDateWrap">
               <h3 className="jobTitle">{previewData[child]["Role"]}</h3>
               <p className="jobLoc">I {previewData[child]["Company"]}</p>
-              <p className="date">{previewData[child]["StartDate"]} - {previewData[child]["EndDate"]}</p>
+              <p className="date">
+                {previewData[child]["StartDate"]} -{" "}
+                {previewData[child]["EndDate"]}
+              </p>
             </div>
             <ul className="description">
               <li>hello</li>
               <li>there</li>
             </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function EduBuilder({ previewData }) {
+  const expChild = [...previewData[2].children];
+
+  if (!expChild.length) return null;
+
+  return (
+    <div className="eduDetails">
+      <h3 className="detailsHeader">Education</h3>
+
+      {expChild.map((child, idx) => {
+        return (
+          <div key={idx} className="education">
+            <div className="edutitleLocWrap">
+              <h3 className="jobTitle">{previewData[child]["Institute"]}</h3>
+              <p className="jobLoc">I {previewData[child]["Location"]}</p>
+              <p className="date">
+                {previewData[child]["StartDate"]} -{" "}
+                {previewData[child]["EndDate"]}
+              </p>
+            </div>
+
+            <p className="degree">{previewData[child]["Degree"]}</p>
+            <p className="year">{previewData[child]["Year"]}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function ProBuilder({ previewData }) {
+  const expChild = [...previewData[3].children];
+
+  if (!expChild.length) return null;
+
+  return (
+    <div className="proDetails">
+      <h3 className="detailsHeader">Projects</h3>
+
+      {expChild.map((child, idx) => {
+        return (
+          <div key={idx} className="education">
+            <div className="projectWraps">
+              <h3 className="jobTitle">{previewData[child]["Title"]}</h3>
+              <p className="jobLoc">I {previewData[child]["TechStack"]}</p>
+            </div>
+            <ul className="description">
+              <li>ffefw</li>
+              <li>dsfwf</li>
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function SkiBuilder({ previewData }) {
+  const expChild = [...previewData[4].children];
+
+  if (!expChild.length) return null;
+
+  return (
+    <div className="proDetails">
+      <h3 className="detailsHeader">Projects</h3>
+
+      {expChild.map((child, idx) => {
+        return (
+          <div key={idx} className="education">
+            <h4 className="stackTitle">{previewData[child]["Title"]}</h4> : <p className="skills">{previewData[child]["skills"]}</p>
           </div>
         );
       })}
