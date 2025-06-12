420800a8a6d44784192e905b6c7cfb80148745e2
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-12 19:51:58 +0530

    shortened code by calling the function inside the component prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import { ResumeEditor } from './components/resumeEditor'
-import { ResumePreview } from './components/resumePreview'
-import { mainData } from './components/mainData';
+import { useState } from "react";
+import { ResumeEditor } from "./components/resumeEditor";
+import { ResumePreview } from "./components/resumePreview";
+import { mainData } from "./components/mainData";
 
-export function App(){
-    const [resumeData, setResumeData] = useState(mainData);
-    
-    function appendingData(givenData){
-        setResumeData(givenData);
-    }
-
-    return <>
-        <ResumeEditor getResumeData = {appendingData} previewData={resumeData} />
-        <ResumePreview previewData = {resumeData} />
+export function App() {
+  const [resumeData, setResumeData] = useState(mainData);
+  return (
+    <>
+      <ResumeEditor
+        getResumeData={(givenData) => setResumeData(givenData)}
+        previewData={resumeData}
+      />
+      <ResumePreview previewData={resumeData} />
     </>
+  );
 }
