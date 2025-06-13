3faa3442f7645d8d73c33527a786d6423844e630
ruchik-panwal <ruchik.panwal@gmail.com>
2025-06-13 23:00:12 +0530

    added local storage function

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import { ResumeEditor } from "./components/resumeEditor";
 import { ResumePreview } from "./components/resumePreview";
 import { mainData } from "./components/mainData";
 
+// Takes the whole data from the form; Changes its state; stores inside local storages; and transfer to the previewDiv
 export function App() {
-  const [resumeData, setResumeData] = useState(mainData);
+  let newData = {};
+  if (!localStorage.getItem("data"))
+    newData = { ...mainData }; //If nothing in local storage, use the main Data
+  else newData = JSON.parse(localStorage.getItem("data")); //Else Use thaat lacal storage
+
+  // Use State for the main data
+  const [resumeData, setResumeData] = useState(newData);
   return (
     <>
       <ResumeEditor
-        getResumeData={(givenData) => setResumeData(givenData)}
+        getResumeData={(givenData) => {
+          setResumeData(givenData); //Getting and setting the data from the forms
+          localStorage.setItem("data", JSON.stringify(givenData)); //Storing inside the local Storage
+        }}
         previewData={resumeData}
       />
-      <ResumePreview previewData={resumeData} />
+      <ResumePreview previewData={resumeData} />{" "}
+      {/* Transfers the Data to preview */}
     </>
   );
 }
